7ca297cc05e6586a1f2a34a5afa357d20d18b07c
Stephen Hwang <stephenhwang@berkeley.edu>
2023-02-23 01:04:25 -0800

    Fixed js

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function bork() {
+    var bork = new Audio("./sound/bork.mp3");
+    bork.play();
+  }
+
+function addnote() {
+  const form = document.querySelector("form");
+  const input = document.querySelector("input");
+  const ul = document.getElementById("notes");
+
+  var bork = new Audio("./sound/bork.mp3");
+  bork.play();
+}
